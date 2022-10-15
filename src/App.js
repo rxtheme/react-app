5dc13f17e0a6270b87c9c4c23b0404b243715b6c
yousufislamme <online.okk@gmail.com>
2022-10-15 21:23:57 +0600

    some chenge App . js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='personStyle'>
+      <Person name='Aouf' desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, sed.'></Person>
+      <Person name='Yousuf' address='Dhaka'></Person>
+      <Person name='Samira'></Person>
+      <Person name='Anas'></Person>
     </div>
   );
+}
+
+function Person(props) {
+  const personStyle = {
+    border: '2px solid red',
+    margin: "20px auto",
+    padding: '10px',
+    width: '400px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name: {props.name} </h1>
+      <h4>Hello {props.address}</h4>
+      <p>{props.desc}Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, sed.</p>
+
+    </div>
+  )
 }
 
 export default App;
